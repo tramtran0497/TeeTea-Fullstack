a6529c21bbc1a30e44f89d59858b68cb41810fee
tramtran0497 <tramtran0497@gmail.com>
2022-05-30 11:53:43 +0300

    Heroku deploy settings

--- api/src/db/mongoose.js
+++ api/src/db/mongoose.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose")
 
 // console.log(process.env.MONGODB_URL)
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-})  .then(() => console.log("mongoDB is connected"))
-    .catch((err) => console.log(err));
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true 
+// })  .then(() => console.log("mongoDB is connected"))
+//     .catch((err) => console.log(err));
 
-// mongoose.connect(process.env.MONGODB_URL_CLOUD)
-//     .then(() => console.log("Database is connected!"))
-//     .catch(err => console.log("Database connection gets error!", err))
+mongoose.connect(process.env.MONGODB_URL_CLOUD)
+    .then(() => console.log("Database is connected!"))
+    .catch(err => console.log("Database connection gets error!", err))
