3669700fa43ee10b9db2ebff5d007972c23da85f
tramtran0497 <tramtran0497@gmail.com>
2022-08-20 12:44:41 +0300

    changes made to the project

--- api/src/app.js
+++ api/src/app.js
@@ -7,13 +7,19 @@ const newsRouter = require("./routers/newsRouter");
 const orderRouter = require("./routers/orderRouter");
 const userRouter = require("./routers/userRouter");
 
+const cors = require('cors');
+
 const app = express();
 // Middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "Access-Control-Allow-Headers");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.ALLOW_CORS.split(", ")
+}));
 
 app.use(productRouter);
 app.use(eventRouter);
